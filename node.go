38df8ed94e4085803ef4c80d415ddca14ef3348e
Tom Cully <tom.cully@apitalent.co.nz>
2016-10-08 18:13:12 +1300

    Gofmt entire project

--- node.go
+++ node.go
@@ -1,109 +1,144 @@
 package freespacetree
 
 type Node struct {
-  from uint64
-  to uint64
-  left *Node
-  right *Node
+	from  uint64
+	to    uint64
+	left  *Node
+	right *Node
 }
 
 func NewNode(from, to uint64) *Node {
-  inst := &Node{
-    left: nil,
-    right: nil,
-    from: from,
-    to: to,
-  }
-  return inst
+	inst := &Node{
+		left:  nil,
+		right: nil,
+		from:  from,
+		to:    to,
+	}
+	return inst
 }
 
 func (me *Node) Allocate(blocks uint64) (uint64, bool) {
-  if me.left !=nil { 
-    blockid, found := me.left.Allocate(blocks)
-    if found { return blockid, found }
-  } 
-  if me.to-me.from >= blocks {
-    // Will fit in this node.
-    blockid := me.from
-    me.from += blocks
-    return blockid, true
-  }
-  if me.right !=nil {
-    blockid, found := me.right.Allocate(blocks)
-    if found { return blockid, found }
-  } 
+	if me.left != nil {
+		blockid, found := me.left.Allocate(blocks)
+		if found {
+			return blockid, found
+		}
+	}
+	if me.to-me.from >= blocks {
+		// Will fit in this node.
+		blockid := me.from
+		me.from += blocks
+		return blockid, true
+	}
+	if me.right != nil {
+		blockid, found := me.right.Allocate(blocks)
+		if found {
+			return blockid, found
+		}
+	}
 
-  return 0, false
+	return 0, false
 }
 
 func (me *Node) Deallocate(blockid uint64, blocklength uint64) *Node {
-  node := NewNode(blockid, blockid+blocklength)
-  me.AddNode(node)
-  return me
+	node := NewNode(blockid, blockid+blocklength)
+	me.AddNode(node)
+	return me
 }
 
 // Add an existing node into the tree, merging if necessary
 // Returns the new root of the tree
 func (me *Node) AddNode(node *Node) *Node {
-  // Detect node engulfed by me
-  if me.from <= node.from && me.to >= node.to {
-    // Add node's children if any
-    if node.left != nil { me.AddNode(node.left) }
-    if node.right != nil { me.AddNode(node.right) }
-    return me // drop node
-  }
-  // Detect me engulfed by node
-  if node.from <= me.from && node.to >= me.to {
-    // add our children to new node
-    if me.left!=nil { node.AddNode(me.left) }
-    if me.right!=nil { node.AddNode(me.right) }
-    // drop me, return new node
-    me.left = nil; me.right = nil
-    return node
-  }
-  // Detect adjacent to left / overlaps left
-  if node.to == me.from-1 || (node.from <= me.from && node.to <= me.to && node.to >= me.from) {
-    me.from = node.from // extend me and drop new node
-    // Add node's children if any
-    if node.left != nil { me.AddNode(node.left) }
-    if node.right != nil { me.AddNode(node.right) }
-    // Clear and re-add children
-    left := me.left; right := me.right
-    me.left = nil; me.right = nil
-    if left != nil { me.AddNode(left) }
-    if right != nil { me.AddNode(right) }
-    // drop node
-    node.left = nil; node.right = nil
-    return me
-  }
-  // Detect adjacent to right / overlaps right
-  if node.from == me.to+1 || (node.from >= me.from && node.from <= me.to && node.to <= me.from) {
-    me.to = node.to // extend me
-    // Add node's children if any
-    if node.left != nil { me.AddNode(node.left) }
-    if node.right != nil { me.AddNode(node.right) }
-    // Clear and re-add children
-    left := me.left; right := me.right
-    me.left = nil; me.right = nil
-    if left != nil { me.AddNode(left) }
-    if right != nil { me.AddNode(right) }
-    // drop node
-    node.left = nil; node.right = nil
-    return me
-  }
-  // else, binary insert
-  if node.to < me.from {
-    if me.left == nil {
-      me.left = node
-    } else { 
-      me.left = me.left.AddNode(node)
-    }
-  } else {
-    if me.right == nil {
-      me.right = node
-    } else { 
-      me.right = me.right.AddNode(node)
-    }
-  }
-  return me
+	// Detect node engulfed by me
+	if me.from <= node.from && me.to >= node.to {
+		// Add node's children if any
+		if node.left != nil {
+			me.AddNode(node.left)
+		}
+		if node.right != nil {
+			me.AddNode(node.right)
+		}
+		return me // drop node
+	}
+	// Detect me engulfed by node
+	if node.from <= me.from && node.to >= me.to {
+		// add our children to new node
+		if me.left != nil {
+			node.AddNode(me.left)
+		}
+		if me.right != nil {
+			node.AddNode(me.right)
+		}
+		// drop me, return new node
+		me.left = nil
+		me.right = nil
+		return node
+	}
+	// Detect adjacent to left / overlaps left
+	if node.to == me.from-1 || (node.from <= me.from && node.to <= me.to && node.to >= me.from) {
+		me.from = node.from // extend me and drop new node
+		// Add node's children if any
+		if node.left != nil {
+			me.AddNode(node.left)
+		}
+		if node.right != nil {
+			me.AddNode(node.right)
+		}
+		// Clear and re-add children
+		left := me.left
+		right := me.right
+		me.left = nil
+		me.right = nil
+		if left != nil {
+			me.AddNode(left)
+		}
+		if right != nil {
+			me.AddNode(right)
+		}
+		// drop node
+		node.left = nil
+		node.right = nil
+		return me
+	}
+	// Detect adjacent to right / overlaps right
+	if node.from == me.to+1 || (node.from >= me.from && node.from <= me.to && node.to <= me.from) {
+		me.to = node.to // extend me
+		// Add node's children if any
+		if node.left != nil {
+			me.AddNode(node.left)
+		}
+		if node.right != nil {
+			me.AddNode(node.right)
+		}
+		// Clear and re-add children
+		left := me.left
+		right := me.right
+		me.left = nil
+		me.right = nil
+		if left != nil {
+			me.AddNode(left)
+		}
+		if right != nil {
+			me.AddNode(right)
+		}
+		// drop node
+		node.left = nil
+		node.right = nil
+		return me
+	}
+	// else, binary insert
+	if node.to < me.from {
+		if me.left == nil {
+			me.left = node
+		} else {
+			me.left = me.left.AddNode(node)
+		}
+	} else {
+		if me.right == nil {
+			me.right = node
+		} else {
+			me.right = me.right.AddNode(node)
+		}
+	}
+	return me
 }
--- node_test.go
+++ node_test.go
@@ -1,203 +1,196 @@
 package freespacetree
 
 import (
-  "testing"
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestNode(t *testing.T) {
-  x := NewNode(5,7)
+	x := NewNode(5, 7)
 
-  assert.NotNil(t, x)
-  assert.Equal(t, x.from, uint64(5))
-  assert.Equal(t, x.to, uint64(7))
+	assert.NotNil(t, x)
+	assert.Equal(t, x.from, uint64(5))
+	assert.Equal(t, x.to, uint64(7))
 }
 
 func TestAllocate(t *testing.T) {
-  // Striaghtforward alloc
-  root := NewNode(0,20)
+	// Striaghtforward alloc
+	root := NewNode(0, 20)
 
-  id, ok := root.Allocate(10)
-  assert.Equal(t, id, uint64(0))
-  assert.Equal(t, root.from, uint64(10))
-  assert.Equal(t, root.to, uint64(20))
-  assert.Nil(t, root.left)
-  assert.Nil(t, root.right)
-  assert.True(t, ok)
+	id, ok := root.Allocate(10)
+	assert.Equal(t, id, uint64(0))
+	assert.Equal(t, root.from, uint64(10))
+	assert.Equal(t, root.to, uint64(20))
+	assert.Nil(t, root.left)
+	assert.Nil(t, root.right)
+	assert.True(t, ok)
 
-  id, ok = root.Allocate(10)
-  assert.Equal(t, id, uint64(10))
-  assert.Equal(t, root.from, uint64(20))
-  assert.Equal(t, root.to, uint64(20))
-  assert.Nil(t, root.left)
-  assert.Nil(t, root.right)
-  assert.True(t, ok)
-  
-  id, ok = root.Allocate(10)
-  assert.Equal(t, id, uint64(0))
-  assert.False(t, ok)
+	id, ok = root.Allocate(10)
+	assert.Equal(t, id, uint64(10))
+	assert.Equal(t, root.from, uint64(20))
+	assert.Equal(t, root.to, uint64(20))
+	assert.Nil(t, root.left)
+	assert.Nil(t, root.right)
+	assert.True(t, ok)
+
+	id, ok = root.Allocate(10)
+	assert.Equal(t, id, uint64(0))
+	assert.False(t, ok)
 }
 
 func TestFillAllocate(t *testing.T) {
-  root := NewNode(0,20)
+	root := NewNode(0, 20)
 
-  id, ok := root.Allocate(15)
-  assert.Equal(t, id, uint64(0))
-  assert.Equal(t, root.from, uint64(15))
-  assert.Equal(t, root.to, uint64(20))
-  assert.Nil(t, root.left)
-  assert.Nil(t, root.right)
-  assert.True(t, ok)
+	id, ok := root.Allocate(15)
+	assert.Equal(t, id, uint64(0))
+	assert.Equal(t, root.from, uint64(15))
+	assert.Equal(t, root.to, uint64(20))
+	assert.Nil(t, root.left)
+	assert.Nil(t, root.right)
+	assert.True(t, ok)
 
-  id, ok = root.Allocate(5)
-  assert.Equal(t, id, uint64(15))
-  assert.Equal(t, root.from, uint64(20))
-  assert.Equal(t, root.to, uint64(20))
-  assert.Nil(t, root.left)
-  assert.Nil(t, root.right)
-  assert.True(t, ok)
+	id, ok = root.Allocate(5)
+	assert.Equal(t, id, uint64(15))
+	assert.Equal(t, root.from, uint64(20))
+	assert.Equal(t, root.to, uint64(20))
+	assert.Nil(t, root.left)
+	assert.Nil(t, root.right)
+	assert.True(t, ok)
 
-  root = root.Deallocate(0,5)
-  assert.NotNil(t, root)
-  assert.NotNil(t, root.left)
-  assert.Equal(t, root.left.from, uint64(0))
-  assert.Equal(t, root.left.to, uint64(5))
+	root = root.Deallocate(0, 5)
+	assert.NotNil(t, root)
+	assert.NotNil(t, root.left)
+	assert.Equal(t, root.left.from, uint64(0))
+	assert.Equal(t, root.left.to, uint64(5))
 }
 
 func TestAddNodeLeftAdjacentMerge(t *testing.T) {
-  root := NewNode(40,50)
-  root.AddNode(NewNode(100,110))
-  node := NewNode(30,39)
-  node.AddNode(NewNode(0,10))
-  node.AddNode(NewNode(45,50))
+	root := NewNode(40, 50)
+	root.AddNode(NewNode(100, 110))
+	node := NewNode(30, 39)
+	node.AddNode(NewNode(0, 10))
+	node.AddNode(NewNode(45, 50))
 
-  root = root.AddNode(node)
-  assert.Equal(t, root.from, uint64(30))
-  assert.Equal(t, root.to, uint64(50))
-  assert.NotNil(t, root.left)
-  assert.Nil(t, root.left.left)
-  assert.Nil(t, root.left.right)
-  assert.NotNil(t, root.right)
+	root = root.AddNode(node)
+	assert.Equal(t, root.from, uint64(30))
+	assert.Equal(t, root.to, uint64(50))
+	assert.NotNil(t, root.left)
+	assert.Nil(t, root.left.left)
+	assert.Nil(t, root.left.right)
+	assert.NotNil(t, root.right)
 }
 
 func TestAddNodeRightAdjacentMerge(t *testing.T) {
-  root := NewNode(40,50)
-  root.AddNode(NewNode(10,20))
-  
-  node := NewNode(51,60)
-  node.AddNode(NewNode(80,90))
-  node.AddNode(NewNode(45,46))
+	root := NewNode(40, 50)
+	root.AddNode(NewNode(10, 20))
 
-  root = root.AddNode(node)
-  assert.Equal(t, root.from, uint64(40))
-  assert.Equal(t, root.to, uint64(60))
-  assert.NotNil(t, root.left)
-  assert.NotNil(t, root.right)
+	node := NewNode(51, 60)
+	node.AddNode(NewNode(80, 90))
+	node.AddNode(NewNode(45, 46))
+
+	root = root.AddNode(node)
+	assert.Equal(t, root.from, uint64(40))
+	assert.Equal(t, root.to, uint64(60))
+	assert.NotNil(t, root.left)
+	assert.NotNil(t, root.right)
 }
 
 func TestAddNodeEngulfs(t *testing.T) {
-  root := NewNode(0,20)
-  node := NewNode(10,15)
+	root := NewNode(0, 20)
+	node := NewNode(10, 15)
 
-  root = root.AddNode(node)
-  assert.Equal(t, root.from, uint64(0))
-  assert.Equal(t, root.to, uint64(20))
-  assert.Nil(t, root.left)
-  assert.Nil(t, root.right)
+	root = root.AddNode(node)
+	assert.Equal(t, root.from, uint64(0))
+	assert.Equal(t, root.to, uint64(20))
+	assert.Nil(t, root.left)
+	assert.Nil(t, root.right)
 }
 
 func TestAddNodeIsEngulfedNoChildren(t *testing.T) {
-  root := NewNode(10,15)
-  node := NewNode(0, 25)
+	root := NewNode(10, 15)
+	node := NewNode(0, 25)
 
-  root = root.AddNode(node)
-  assert.Equal(t, root.from, uint64(0))
-  assert.Equal(t, root.to, uint64(25))
-  assert.Nil(t, root.left)
-  assert.Nil(t, root.right)
+	root = root.AddNode(node)
+	assert.Equal(t, root.from, uint64(0))
+	assert.Equal(t, root.to, uint64(25))
+	assert.Nil(t, root.left)
+	assert.Nil(t, root.right)
 }
 
 func TestAddNodeIsEngulfedChildren(t *testing.T) {
-  root := NewNode(10,15)
-  root.AddNode(NewNode(0,1))
-  root.AddNode(NewNode(22,23))
+	root := NewNode(10, 15)
+	root.AddNode(NewNode(0, 1))
+	root.AddNode(NewNode(22, 23))
 
-  node := NewNode(5, 20)
+	node := NewNode(5, 20)
 
-  root = root.AddNode(node)
-  assert.Equal(t, root.from, uint64(5))
-  assert.Equal(t, root.to, uint64(20))
-  assert.NotNil(t, root.left)
-  assert.NotNil(t, root.right)
-  assert.Equal(t, root.left.from, uint64(0))
-  assert.Equal(t, root.left.to, uint64(1))
-  assert.Equal(t, root.right.from, uint64(22))
-  assert.Equal(t, root.right.to, uint64(23))
+	root = root.AddNode(node)
+	assert.Equal(t, root.from, uint64(5))
+	assert.Equal(t, root.to, uint64(20))
+	assert.NotNil(t, root.left)
+	assert.NotNil(t, root.right)
+	assert.Equal(t, root.left.from, uint64(0))
+	assert.Equal(t, root.left.to, uint64(1))
+	assert.Equal(t, root.right.from, uint64(22))
+	assert.Equal(t, root.right.to, uint64(23))
 }
 
 func TestAddNodeDepth(t *testing.T) {
-  root := NewNode(40,50)
-  root.AddNode(NewNode(20,30))
-  root.AddNode(NewNode(0,10))
+	root := NewNode(40, 50)
+	root.AddNode(NewNode(20, 30))
+	root.AddNode(NewNode(0, 10))
 
-  root.AddNode(NewNode(60,70))
-  root.AddNode(NewNode(80,90))
+	root.AddNode(NewNode(60, 70))
+	root.AddNode(NewNode(80, 90))
 
-  assert.NotNil(t, root.left)
-  assert.NotNil(t, root.right)
-  assert.NotNil(t, root.left.left)
-  assert.Nil(t, root.left.right)
-  assert.NotNil(t, root.right.right)
-  assert.Nil(t, root.right.left)
+	assert.NotNil(t, root.left)
+	assert.NotNil(t, root.right)
+	assert.NotNil(t, root.left.left)
+	assert.Nil(t, root.left.right)
+	assert.NotNil(t, root.right.right)
+	assert.Nil(t, root.right.left)
 }
 
 func TestAddNodeDepthEngulf(t *testing.T) {
-  root := NewNode(40,50)
-  root.AddNode(NewNode(0,10))
-  root.AddNode(NewNode(20,30))
+	root := NewNode(40, 50)
+	root.AddNode(NewNode(0, 10))
+	root.AddNode(NewNode(20, 30))
 
-  root.AddNode(NewNode(60,70))
-  root.AddNode(NewNode(80,90))
+	root.AddNode(NewNode(60, 70))
+	root.AddNode(NewNode(80, 90))
 
-  root.AddNode(NewNode(0,30))
+	root.AddNode(NewNode(0, 30))
 
-  assert.NotNil(t, root.left)
-  assert.NotNil(t, root.right)
-  assert.Nil(t, root.left.left)
-  assert.Nil(t, root.left.right)
-  assert.NotNil(t, root.right.right)
-  assert.Nil(t, root.right.left)
+	assert.NotNil(t, root.left)
+	assert.NotNil(t, root.right)
+	assert.Nil(t, root.left.left)
+	assert.Nil(t, root.left.right)
+	assert.NotNil(t, root.right.right)
+	assert.Nil(t, root.right.left)
 }
 
 func TestAllocateLeftEnd(t *testing.T) {
-  root := NewNode(40,50)
+	root := NewNode(40, 50)
 
-  root.AddNode(NewNode(60,70))
-  root.AddNode(NewNode(80,180))
+	root.AddNode(NewNode(60, 70))
+	root.AddNode(NewNode(80, 180))
 
-  block1, ok := root.Allocate(100)
-  assert.True(t, ok)
-  assert.Equal(t, block1, uint64(80))
+	block1, ok := root.Allocate(100)
+	assert.True(t, ok)
+	assert.Equal(t, block1, uint64(80))
 
-  root.Deallocate(60,40)
+	root.Deallocate(60, 40)
 }
 
 func TestAllocateRightEnd(t *testing.T) {
-  root := NewNode(40,50)
+	root := NewNode(40, 50)
 
-  root.AddNode(NewNode(20,30))
-  root.AddNode(NewNode(0,5))
+	root.AddNode(NewNode(20, 30))
+	root.AddNode(NewNode(0, 5))
 
-  block1, ok := root.Allocate(5)
-  assert.True(t, ok)
-  assert.Equal(t, block1, uint64(0))
+	block1, ok := root.Allocate(5)
+	assert.True(t, ok)
+	assert.Equal(t, block1, uint64(0))
 
-  root.Deallocate(60,40)
+	root.Deallocate(60, 40)
 }
-
-
-
-
-
-
-
